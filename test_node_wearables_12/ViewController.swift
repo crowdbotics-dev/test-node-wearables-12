ca52f9ea906ea4c3fcde88f934fa18843fc8dddc
Crowdbotics <engineering@crowdbotics.com>
2022-09-07 15:38:44 +0000

    Initial commit.

---
+++ test_node_wearables_12/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test_node_wearables_12
+//
+//  Created by Dan on 10/23/18.
+//  Copyright © 2018 Crowdbotics. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
